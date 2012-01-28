a7811a33d339b74b4b9a895d16336b3d68273870
Kenny Yu <yu.kenny124@gmail.com>
2012-01-27 21:47:13 -0500

    Initial Drop

---
+++ init.el
@@ -0,0 +1,74 @@
+;; insert spaces instead of tabs
+(setq-default indent-tabs-mode nil)
+
+;; show column numbers
+(column-number-mode t)
+
+;; show matching parens
+(show-paren-mode 1)
+
+;; show time
+(display-time)
+
+;; enable mouse clicking in emacs
+(require 'mouse)
+(xterm-mouse-mode t)
+(defun track-mouse (e))
+(setq mouse-sel-mode t)
+
+;; tuareg mode
+(add-to-list 'load-path "~/.emacs.d/tuareg-2.0.4")
+(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+(autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+(setq auto-mode-alist 
+      (append '(("\\.ml[ily]?$" . tuareg-mode)
+		("\\.topml$" . tuareg-mode))
+	      auto-mode-alist))
+
+;; color theme
+(add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
+(add-to-list 'load-path "~/.emacs.d/emacs-color-theme-solarized")
+(require 'color-theme-solarized)
+(color-theme-solarized-dark)
+
+;; line numbers
+(add-to-list 'load-path "~/.emacs.d")
+(require 'linum)
+(global-linum-mode 1)
+
+;; directory trees
+(autoload 'dirtree "dirtree" "Add directory to tree view" t)
+
+;; highlight 80 characters
+(require 'highlight-80+)
+(add-hook 'find-file-hook 'highlight-80+-mode)
+
+;; delete extra whitespace
+(defun duplicate-line (arg)
+  "Duplicate current line, leaving point in lower line."
+  (interactive "*p")
+  ;; save the point for undo
+  (setq buffer-undo-list (cons (point) buffer-undo-list))
+  ;; local variables for start and end of line
+  (let ((bol (save-excursion (beginning-of-line) (point)))
+        eol)
+    (save-excursion
+      ;; don't use forward-line for this, because you would have
+      ;; to check whether you are at the end of the buffer
+      (end-of-line)
+      (setq eol (point))
+      ;; store the line and disable the recording of undo information
+      (let ((line (buffer-substring bol eol))
+            (buffer-undo-list t)
+            (count arg))
+        ;; insert the line arg times
+        (while (> count 0)
+          (newline)         ;; because there is no newline in 'line'
+          (insert line)
+          (setq count (1- count)))
+        )
+      ;; create the undo information
+      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
+    ) ; end-of-let
+  ;; put the point in the lowest line and return
+  (next-line arg))
